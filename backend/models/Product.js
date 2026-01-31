48cefde05d0c172f038d378937312ffdf7c5e976
Srihari Acharya <158181444+Srihariacharya@users.noreply.github.com>
2026-01-31 16:36:37 +0530

    Add Product and Order models

---
+++ backend/models/Product.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0
+  }
+});
+
+module.exports = mongoose.model("Product", productSchema);
